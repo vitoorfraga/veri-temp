069a67cf7d41e67348ff497319a85f069a483b47
Vitor Fraga <vitorfragaps@gmail.com>
2024-02-23 23:33:59 -0300

    feat: add DefaultLayout

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,13 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { HomePage } from '../pages/HomePage'
+import { DefaultLayout } from '../layouts/DefaultLayout'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/history',
-    element: <h1>History Page</h1>,
+    element: <DefaultLayout />, // 👈 Layout que será aplicado em todas as páginas.
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/history',
+        element: <h1>History Page</h1>,
+      },
+    ],
   },
 ])
