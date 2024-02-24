ed791604f1b473b3bb7edb0b12fbc4b87582773f
Vitor Fraga <vitorfragaps@gmail.com>
2024-02-24 16:35:35 -0300

    chore: setup and initialize React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './route'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 function App() {
-  return <RouterProvider router={router} />
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
