c969f33262121cf4bdaea6abee3316e4c0d1f389
Vitor Fraga <vitorfragaps@gmail.com>
2024-02-25 15:31:51 -0300

    refactor: transform Button component in Forward Component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,21 @@
-import { ButtonHTMLAttributes } from 'react'
+import { ButtonHTMLAttributes, forwardRef, ReactNode } from 'react'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode
+  children: ReactNode
 }
 
-export const Button = ({ children, ...rest }: ButtonProps) => {
-  return (
-    <button
-      className="py-2 px-4 bg-sky-100 rounded-lg flex items-center gap-10 hover:bg-sky-200 cursor-pointer transition-colors"
-      {...rest}
-    >
-      {children}
-    </button>
-  )
-}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <button
+        className="py-2 px-4 bg-sky-100 rounded-lg flex items-center gap-10 hover:bg-sky-200 cursor-pointer transition-colors"
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </button>
+    )
+  },
+)
+
+Button.displayName = 'Button'
