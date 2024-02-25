1c7fa61199f6806ba4e228f1b571d4f684a4c299
Vitor Fraga <vitorfragaps@gmail.com>
2024-02-25 15:36:47 -0300

    feat: add PageTitle component

---
+++ src/components/PageTitle/index.tsx
@@ -0,0 +1,15 @@
+import { HTMLAttributes, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface PageTitleProps extends HTMLAttributes<HTMLHeadingElement> {
+  children: ReactNode
+  className?: string
+}
+
+export const PageTitle = ({ children, className }: PageTitleProps) => {
+  return (
+    <h1 className={twMerge('font-bold text-4xl text-sky-950', className)}>
+      {children}
+    </h1>
+  )
+}
