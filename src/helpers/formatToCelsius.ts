71fb8065e058bcbfa31d34c0b069826213453232
Vitor Fraga <vitorfragaps@gmail.com>
2024-02-26 02:35:54 -0300

    fix: change the formatting style in formatToCelsius

--- src/helpers/formatToCelsius.ts
+++ src/helpers/formatToCelsius.ts
@@ -1,12 +1,16 @@
 // 👉🏻 Converte um número para Celsius.
 
-export const FormatToCelsius = (temp: number) => {
-  const formattedTempToCelcius = temp.toLocaleString('pt-br', {
-    style: 'unit',
+export const FormatToCelsius = (temp: number | null | undefined) => {
+  // 👉🏻 Verifica se a temperatura existe.
+  const tempExists = !!temp
+  if (!tempExists) return '-'
+
+  const formattedTempToCelcius = temp!.toLocaleString('pt-br', {
     unit: 'celsius',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   })
 
-  return formattedTempToCelcius
+  // 👉🏻 Retorna a temperatura formatada.
+  return `${formattedTempToCelcius}°C`
 }
